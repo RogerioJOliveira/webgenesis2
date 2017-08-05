2c4275fec4bf927569029bcaa8631c25a957a158
xb011708 <p000rogerio.oliveira@prservicos.com.br>
2017-08-05 12:46:18 -0300

    Alteração de Entidade, tipoPessoa e tipoEntidade

--- common/models/entidade.js
+++ common/models/entidade.js
@@ -1,5 +1,20 @@
 'use strict';
 
 module.exports = function(entidade) {
+/*..
+  entidade.findById({include:['tipoPessoa']}, function(data) {
+     console.log(data);
+  });
+*/
+  //console.log(entidade);
 
+  entidade.beforeRemote( '**', function( ctx, user, next) {
+      //...
+      //console.log(ctx.req.body);
+      entidade.find({include:['tipoPessoa','tipoEntidade']}, function(data) {
+        console.log(data);
+      });
+
+      next();
+  });
 };
