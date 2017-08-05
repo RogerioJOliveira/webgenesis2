91ccd9ec6f375125adec96220511cf2cc8b4f256
xb011708 <p000rogerio.oliveira@prservicos.com.br>
2017-08-05 17:22:53 -0300

    Include - Com Sucesso

--- common/models/entidade.js
+++ common/models/entidade.js
@@ -2,19 +2,40 @@
 
 module.exports = function(entidade) {
 /*..
-  entidade.findById({include:['tipoPessoa']}, function(data) {
+  Model.findById({include:['tipoPessoa']}, function(data) {
      console.log(data);
   });
-*/
-  //console.log(entidade);
 
-  entidade.beforeRemote( '**', function( ctx, user, next) {
-      //...
-      //console.log(ctx.req.body);
-      entidade.find({include:['tipoPessoa','tipoEntidade']}, function(data) {
-        console.log(data);
-      });
+  Book.observe('access', function logQuery(ctx, next) {
+    ctx.query.where.id = 2; // changing filter value for where
+  console.log('Accessing %s matching %j', ctx.Model.ModelName, ctx.query.where);
+  next();
+});
 
-      next();
+module.exports = function(User) {
+  User.afterRemote('*', function(ctx, user, next) {
+    User.include(user, 'group', next);
   });
+};
+  entidade.afterRemote ('**', function (context, modelInstance, next) {
+    let ownerId = context.result.ownerId;
+    entidade.find({include:['tipoPessoa','tipoEntidade']}, function(err, owner) {
+      let ownerId = context.result.ownerId;
+      next();
+    });
+  });
+
+*/
+
+  entidade.beforeRemote('**', function(ctx, unused, next) {
+    if (ctx.args.filter === undefined) ctx.args.filter = {};
+    if (ctx.args.filter.include === undefined) ctx.args.filter.include = [];
+    ctx.args.filter.include.push('tipoPessoa');
+    ctx.args.filter.include.push('tipoEntidade');
+
+    console.log('Accessing %s matching %j', 'entidade', ctx.args.filter);
+
+    next();
+  });
+
 };
